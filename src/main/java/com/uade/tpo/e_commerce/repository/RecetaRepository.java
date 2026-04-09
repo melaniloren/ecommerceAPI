ffd546591f6c6dae1bac802b87de1570b102d467
joaquinmonte <166743746+joaquinmonte@users.noreply.github.com>
2026-04-09 16:03:56 -0300

    agregar repository de recetas y recetadetalle

---
+++ src/main/java/com/uade/tpo/e_commerce/repository/RecetaRepository.java
@@ -0,0 +1,8 @@
+package com.uade.tpo.e_commerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.uade.tpo.e_commerce.model.Receta;
+
+public interface RecetaRepository extends JpaRepository<Receta, Long> {
+
+}
