1b422170a595846bbf8a843f0f7af2e15868c79b
joaquinmonte <joaqui.monteverde@gmail.com>
2026-05-15 14:23:05 -0300

    cambio en la config de seguridad

--- src/main/java/com/uade/tpo/e_commerce/config/SecurityConfig.java
+++ src/main/java/com/uade/tpo/e_commerce/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.uade.tpo.e_commerce.config;
 
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.uade.tpo.e_commerce.exception.EmailNotFoundException;
 import com.uade.tpo.e_commerce.model.Rol;
@@ -21,153 +25,158 @@ import com.uade.tpo.e_commerce.security.JwtFilter;
 
 import lombok.RequiredArgsConstructor;
 
-// Indica que esta clase contiene configuraciones de Spring
 @Configuration
-// Habilita la seguridad web de Spring Security
 @EnableWebSecurity
-// Esto no es Spring Security: Genera un constructor con los campos final requeridos lombok 
 @RequiredArgsConstructor
 public class SecurityConfig {
-    
-    private final JwtFilter jwtFilter;
 
+    private final JwtFilter jwtFilter;
     private final UsuarioRepository usuarioRepository;
 
-    // Cargar los datos del usuario desde tu sistema a través de UsuarioRepository
-    //lo utiliza AuthenticationService para buscar el email
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> usuarioRepository.findByEmail(username)
                 .orElseThrow(() -> new EmailNotFoundException("email", username));
     }
 
-    // Recibe las credenciales del usuario (a través del UsernamePasswordAuthenticationToken)
-    // Usa el UserDetailsService para buscar el usuario en la base de datos
-    // Usa el PasswordEncoder para verificar si la contraseña proporcionada coincide con la almacenada
-    // Si todo es correcto, crea un token de autenticación; si no, lanza una excepción    
-    // @Bean
-    // public AuthenticationProvider authenticationProvider() {
-    //     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    //     authProvider.setPasswordEncoder(passwordEncoder());
-    //     authProvider.setUserDetailsService(userDetailsService());
-    //     return authProvider;
-    // }
-
-    /**
-     * AuthenticationManager es el componente central de autenticación en Spring Security.
-     * 
-     * Funcionamiento:
-     * 1. Recibe un objeto Authentication (UsernamePasswordAuthenticationToken en nuestro caso)
-     * 2. Delega la autenticación a una cadena de AuthenticationProvider configurados
-     * 3. Por defecto, usa DaoAuthenticationProvider que:
-     *    - Utiliza UserDetailsService para cargar el usuario de la base de datos
-     *    - Emplea PasswordEncoder para verificar la contraseña
-     *    - Compara las credenciales proporcionadas con las almacenadas
-     * 
-     * Proceso de autenticación:
-     * - Entrada: Credenciales sin verificar (username/password)
-     * - Proceso: Validación de credenciales
-     * - Salida: Authentication completamente autenticado con authorities
-     * 
-     * Si la autenticación falla, lanza AuthenticationException
-     */
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception {
         return config.getAuthenticationManager();
     }
 
-    // Define el codificador de contraseñas que se usará para encriptar y verificar passwords
-    // este encoder lo utiliza AuthenticationService.authenticate para verificar la pass, la encripta y compara con la pass de db
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // Configura las reglas de seguridad para las diferentes rutas de la API
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // http
-        //         .csrf(csrf -> csrf.disable())
-        //         .authorizeHttpRequests(auth -> auth
-        //                 // .requestMatchers("/api/recetas/**").permitAll()
-        //                 .requestMatchers("/api/auth/**").permitAll()
-        //                 .anyRequest().authenticated());
-
-        // return http.build();
 
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
-                        // Rutas públicas que no requieren autenticación
-                        //el controller /api/auth puede ser solicitado por cualquier usuario
+
+                        // AUTH
                         .requestMatchers("/api/auth/**").permitAll()
-                        // los endpoints /api/recetas y /api/receta-detalles
 
-
-                        // con método GET son públicos, cualquiera puede ver las recetas
+                        // GET públicos
                         .requestMatchers(HttpMethod.GET, "/api/recetas/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/receta-detalles/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/categorias/**").permitAll() // los usuarios pueden ver el listado de usuarios, pero no los detalles
-                        .requestMatchers(HttpMethod.GET,"/api/pedidos/**").authenticated() //los usuarios pueden ver sus pedidos, pero no los de otros usuarios
-                        .requestMatchers(HttpMethod.GET,"/api/ingredientes/**").permitAll() // los ingredientes son públicos, cualquiera puede verlos
-                        .requestMatchers(HttpMethod.GET,"/api/detalle-pedidos/**").authenticated() // los usuarios pueden ver el detalle de sus pedidos, pero no los de otros usuarios
-                       // .requestMatchers(HttpMethod.GET,"/api/roles/**").hasRole(Rol.ADMIN.name()) TODO: revisar esto si hay parta de rol, faltaria el controller sino
-                        
-                        // Rutas que requieren rol ADMIN para crear/modificar productos
-                        //solo los admins pueden crear un producto
-                        .requestMatchers(HttpMethod.POST, "/api/recetas").hasRole(Rol.ADMIN.name()) //  crea elementos del catálogo y no querés que cualquier usuario publique recetas.
-                        .requestMatchers(HttpMethod.POST, "/api/ingredientes").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.POST, "/api/receta-detalles/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.POST, "/api/pedidos").authenticated() // los usuarios pueden crear pedidos, pero no los de otros usuarios
-                        .requestMatchers(HttpMethod.POST, "/api/detalle-pedidos/**").hasRole(Rol.ADMIN.name()) 
-                        .requestMatchers(HttpMethod.POST, "/api/categorias/**").hasRole(Rol.ADMIN.name())
-                        
-                        //solo los admins pueden actualizar una entidad
-                        .requestMatchers(HttpMethod.PUT, "/api/recetas/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.PATCH, "/api/recetas/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/ingredientes/**").hasRole(Rol.ADMIN.name()) //cubre los dos endpoints de update ingrediente, el update stock y el update ingrediente 
-                        .requestMatchers(HttpMethod.PUT, "/api/pedidos/**").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/api/detalle-pedidos/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/categorias/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/detalle-pedidos/**").hasRole(Rol.ADMIN.name())
-                        
+                        .requestMatchers(HttpMethod.GET, "/api/receta-detalles/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/categorias/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/ingredientes/**").permitAll()
 
-                        //solo los admins pueden eliminar una entidad
-                        .requestMatchers(HttpMethod.DELETE, "/api/recetas/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/ingredientes/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/receta-detalles/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/pedidos/**").authenticated() // Usuarios pueden borrar su pedido, admins tambien
-                        .requestMatchers(HttpMethod.DELETE, "/api/detalle-pedidos/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/categorias/**").hasRole(Rol.ADMIN.name())
+                        // GET autenticados
+                        .requestMatchers(HttpMethod.GET, "/api/pedidos/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/detalle-pedidos/**").authenticated()
 
+                        // POST
+                        .requestMatchers(HttpMethod.POST, "/api/recetas")
+                            .hasRole(Rol.ADMIN.name())
 
-                        // Rutas de usuarios: solo ADMIN
-                        .requestMatchers(HttpMethod.GET, "/api/usuarios/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.POST, "/api/usuarios").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/**").hasRole(Rol.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/api/usuarios/**").hasRole(Rol.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/api/ingredientes")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.POST, "/api/receta-detalles/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.POST, "/api/pedidos")
+                            .authenticated()
+
+                        .requestMatchers(HttpMethod.POST, "/api/detalle-pedidos/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.POST, "/api/categorias/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        // PUT / PATCH
+                        .requestMatchers(HttpMethod.PUT, "/api/recetas/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PATCH, "/api/recetas/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PUT, "/api/ingredientes/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PUT, "/api/pedidos/**")
+                            .authenticated()
+
+                        .requestMatchers(HttpMethod.PUT, "/api/detalle-pedidos/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PUT, "/api/categorias/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        // DELETE
+                        .requestMatchers(HttpMethod.DELETE, "/api/recetas/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/ingredientes/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/receta-detalles/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/pedidos/**")
+                            .authenticated()
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/detalle-pedidos/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/categorias/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        // USUARIOS
+                        .requestMatchers(HttpMethod.GET, "/api/usuarios/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.POST, "/api/usuarios")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.PUT, "/api/usuarios/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/usuarios/**")
+                            .hasRole(Rol.ADMIN.name())
+
+                        // cualquier otra request
                         .anyRequest().authenticated()
                 )
 
-                    
-                        // Cualquier otra ruta requiere autenticación
-                        // con esta linea abarca requiere que todos los endpoints esten autenticados
-                        // no seía necesario post, put, delete /api/recetas , api/pedidos
-                        //.anyRequest().authenticated());
-
-
-                        // insertar un filtro personalizado (su JwtFilter) en la cadena de filtros
-                        // se ejecuta cada vez que se hace una solicitud a un endpoint
-                        // Funcionamiento
-                        // Llegada de la Solicitud: Un cliente envía una solicitud HTTP (por ejemplo, GET /api//products).
-                        // Cadena de Filtros: Spring intercepta la solicitud y la pasa a través de una larga cadena de filtros de seguridad.
-                        // Ejecución del JwtFilter: Como usted lo insertó al inicio de la cadena, su JwtFilter es uno de los primeros en ejecutarse.
-                        // Su método doFilterInternal se ejecuta.
-                        // Si el token es válido: El filtro establece la autenticación en el SecurityContext y llama a filterChain.doFilter(request, response) para pasar la solicitud al siguiente filtro y, finalmente, al controlador.
-                        // Si el token falta o es inválido: El filtro rechaza la solicitud  o deja que la cadena continúe si el endpoint es público.
-                        // Llegada al Controlador: Si el filtro permite el paso, la solicitud finalmente llega a su controlador.
-                        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:5173")
+        );
+
+        configuration.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        );
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
